7bdb5019e7ad000c833e5153dc604313fada03ea
geni <me@geni.site>
2020-02-26 00:30:48 +0200

    Another helpful static assert

--- include/3dstris/util.hpp
+++ include/3dstris/util.hpp
@@ -15,6 +15,9 @@ constexpr T mod(T a, T b) {
 // We're on C++11; no std::make_unique
 template <typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args) {
+	static_assert(std::is_constructible<T, Args...>::value,
+				  "Cannot construct T from passed Args");
+
 	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 

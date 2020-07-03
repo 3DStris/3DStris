e67375dc89c4ad8939a66209fb4afcd83f55f298
geni <me@geni.site>
2020-07-03 18:38:52 +0300

    Add robin-hood-hashing license disclaimer

--- src/util/hash.cpp
+++ src/util/hash.cpp
@@ -1,8 +1,32 @@
+// Hashing taken from https://github.com/martinus/robin-hood-hashing,
+
+// Copyright (c) 2018-2020 Martin Ankerl, 2020 Contributors to the 3DStris
+// project
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+extern "C" {
+#include <3ds/types.h>
+}
+
 #include <string.h>
 
 #include <3dstris/util/hash.hpp>
 
-// Hashing taken from https://github.com/martinus/robin-hood-hashing
 template <typename T>
 inline T unaligned_load(const void* ptr) noexcept {
 	// using memcpy so we don't get into unaligned load problems.

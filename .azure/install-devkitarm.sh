daead76d10faa48ce2d070a8fbf4d810bbefeb47
geni <me@geni.site>
2020-07-03 18:06:41 +0300

    ci: Update devkitPro-pacman

--- .azure/install-devkitarm.sh
+++ .azure/install-devkitarm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-wget https://github.com/devkitPro/pacman/releases/download/devkitpro-pacman-1.0.1/devkitpro-pacman.deb
-sudo dpkg -i devkitpro-pacman.deb
+wget https://github.com/devkitPro/pacman/releases/download/v1.0.2/devkitpro-pacman.amd64.deb
+sudo dpkg -i devkitpro-pacman.amd64.deb
 
 sudo dkp-pacman -Sy
 sudo dkp-pacman -S 3ds-dev --noconfirm
